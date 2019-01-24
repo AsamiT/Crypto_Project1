d38ef6b70fc5fc1cca21b7c031057d7883d7b041
Asami Tachibana <atachibana2612@gmail.com>
2019-01-24 08:29:46 -0500

    Changing that stupid name. My professor is a dillwad.

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/**
+ *
+ * @name: Project-1-Code-Maloy-Robert.cpp
+ * @date: 24 January 2019
+ * @desc: "Implementation of Vignere Cipher in GNU C++"
+ * @author: Robert Maloy
+ *
+ * This program is licensed under the GNU GPL v2.0 license. Please see LICENSE for further details. **/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+	
+	printf("Just testing to see if I'm still sane.");
+	return 0;
+}
+
+
