dfd798929a13349545c11fa3790193f76fcd05f0
Asami Tachibana <atachibana2612@gmail.com>
2019-01-24 08:40:00 -0500

    Not working yet.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 /**
- *
- * @name: Project-1-Code-Maloy-Robert.cpp
+ * @name: main.cpp
  * @date: 24 January 2019
  * @desc: "Implementation of Vignere Cipher in GNU C++"
  * @author: Robert Maloy
@@ -9,12 +8,35 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <string.h>
 
 using namespace std;
+int msgLen, keyLen;
+int i,j;
+
+int getLen(char Set, char Key){
+	msgLen = strlen(Set);
+	keyLen = strlen(Key);
+}
 
 int main() {
 	
-	printf("Just testing to see if I'm still sane.");
+	/* setting up our messages with spaces! */
+	char msg1[] = "A HEALTHY ATTITUDE IS CONTAGIOUS BUT DONT WAIT TO CATCH IT FROM OTHERS.\n";
+	char msg2[] = "IF YOU CARRY YOUR CHILDHOOD WITH YOU YOU NEVER BECOME OLDER.\n";
+	char msg3[] = "FROM PRINCIPLES IS DERIVED PROBABILITY BUT TRUTH OR CERTAINTY IS OBTAINED ONLY FROM FACTS.\n";
+
+	/* our encryption key... */
+	char encKey[] = "MALOY";
+	char testKey[] = "KHOSHAVI"; //this is included in order to test the example test case given in the project PDF. Debug only.
+
+	cout << "Message 1: " << msg1;
+	cout << "Message 2: " << msg2;
+	cout << "Message 3: " << msg3;
+
+	for (i = 0; i < 
+
 	return 0;
 }
 
