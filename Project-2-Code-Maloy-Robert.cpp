ab42e581b036cc8fc05c6296afa40a9c12acd44c
Asami Tachibana <atachibana2612@gmail.com>
2019-02-09 10:50:17 -0500

    New codebase for Project #2.

---
+++ Project-2-Code-Maloy-Robert.cpp
@@ -0,0 +1,17 @@
+/**
+	@name: Project-2-Code-Maloy-Robert.cpp
+	@date: 9 Feb 2019
+	@desc: Implementation of deciphering via XOR into C++
+	@author: Robert Maloy
+	All code is licensed under the GNU GPL v2.0 license. Please refer to LICENSE for more details. **/
+
+//at the moment, it's dummy code!
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	return 0;
+}
+
