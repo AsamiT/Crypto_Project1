1b407fb08b1e03e071ede42176c5653226680ac6
Asami Tachibana <atachibana2612@gmail.com>
2019-02-19 09:41:48 -0500

    We're still not getting much progress.

--- test_set.cpp
+++ test_set.cpp
@@ -18,21 +18,40 @@ using namespace std;
 string cryptPhrases[] = {
     "7370696c796166", //should translate to 'liberty'/'spilyaf'
     "6d796c6c6b7674", //should become 'myllkvt', or 'freedom'.
-    "446f68612c20746c3f20", //"What, me?" or "Doha, tl?"
+    //"446f68612c20746c3f20", //"What, me?" or "Doha, tl?"
 };
 
 using namespace std;
 int lenSet;
+int hex_two;
 size_t size = sizeof(cryptPhrases)/sizeof(cryptPhrases[0]);
+int xorArray[200];
 
 int main() {
     for (int i = 0; i < size; i++) {
         string Input = cryptPhrases[i];
         cout << Input << endl;
-        printf(" --> %lu\n", Input.length());
-        if (sizeof(cryptPhrases[i]) > lenSet) {
-            lenSet = sizeof(cryptPhrases[i]);
+        //printf(" --> %lu\n", Input.length());
+        if (sizeof(Input) > lenSet) {
+            lenSet = sizeof(Input);
+        }
+        int j = 0;
+        while (j < size) {
+            //cout << j << endl;
+            unsigned int hex_one = strtoul(cryptPhrases[i].substr(j, 2).c_str(), NULL, 16);
+            if (hex_two != 0) {
+                //cout << "hex:" << cryptPhrases[i].substr(j, 2).c_str() << endl;
+                //cout << "hex one:" << hex_one << endl;
+                //cout << "hex two:" << hex_two << endl;
+                int xor_val = (hex_one) ^ (hex_two);
+                xor_val += 64;
+                cout << "xor symbol: " << (char)xor_val << endl;
+            }
+            hex_two = hex_one;
+            hex_one = 0;
+            j+=2;
         }
     }
+    
 }
 
