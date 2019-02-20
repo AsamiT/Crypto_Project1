e47aed2d028c6492f200fabc5522cfe040fff26d
Asami Tachibana <atachibana2612@gmail.com>
2019-02-20 09:48:50 -0500

    Still heading nowhere

--- py/xor_series.py
+++ py/xor_series.py
@@ -1,6 +1,8 @@
 # First time writing Python code in quite awhile. Let's see how this goes.
-#import base64?
-import base64
+
+# Project 2 for Applied Cryptography
+# Robert Maloy
+# 9 February 2019
 
 # We're going to declare our strings! What a joyous miracle, no bloody arrays of strings!
 string1="9d6e7a7d155295eef8512c087da56084f743aaa9985ee3848a768c3484d2e2ea6b3f4e5483f61 2d55987ff4782f360bd4809bab835fa1e65f8459c6814472508cc7f72241ee70c34fb9e7c8c4c 246132845d5d73d070de99a73efe1e9ee627ea8f4aaae147087cce6c8cd08813f81caf9d71204 86b16"
@@ -8,7 +10,7 @@ string2="8968617d0f1b97e5ee51280f3aec72caf106bdb48d44a68291339e35db86b6f56f21405
 targetString="866e786a0042d4abf21e305c35ad65c1b542bbbe8f55b3848032c6359fc3e2e96b21421196a10 7c90195a30896bc61f54906abae35fd196fb1519b2d1206320b892b7c7719e60e37b697738c07 76292a9c5d132ac66a8bd1a728bb5882e629"
 
 # testing variables
-enc = 'that'
+enc = 'the of and a to in is be that was he for it with as his I on have at by not they this had are but from or she'
 
 def stringxor(msg1, msg2):
     if len(msg1) > len(msg2):
@@ -19,6 +21,7 @@ def stringxor(msg1, msg2):
 txt = stringxor(string1, string2)
 print(txt)
 
-txtcomp = stringxor(txt, enc)
+txtcomp = stringxor(enc, txt)
 print
+print(enc)
 print(txtcomp)
